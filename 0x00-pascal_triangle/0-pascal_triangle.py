18875c57b6c7cdd03a09f9f37fc4362e392798b3
garikaipaidamoyo <garikaipaidamoyo@gmail.com>
2023-09-30 14:34:07 +0000

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []  # Return an empty list for n <= 0
+    if not isinstance(n, int) or n <= 0:
+        return []  # Return an empty list for non-integer or n <= 0
 
     triangle = []  # Initialize an empty list to store the triangle
 
     for i in range(n):
         row = []  # Initialize a new row for each iteration
-        if i == 0:
-            row.append(1)  # The first row starts with 1
-        else:
-            prev_row = triangle[i - 1]  # Get the previous row
-            row.append(1)  # The first element in each row is 1
-            for j in range(1, i):
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)  # The first and last elements in each row are 1
+            else:
                 # Calculate the elements in between based on the previous row
-                element = prev_row[j - 1] + prev_row[j]
+                element = triangle[i - 1][j - 1] + triangle[i - 1][j]
                 row.append(element)
-            row.append(1)  # The last element in each row is 1
         triangle.append(row)  # Add the row to the triangle
 
     return triangle
