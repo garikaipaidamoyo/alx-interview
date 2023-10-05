cd01b6754aa79328c616f2a724c298ba6049ab0b
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-10-05 01:44:53 -0700

    Correcting errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,20 +3,22 @@
 Module: 0-lockboxes
 """
 
+
 def canUnlockAll(boxes):
     """
     Determines if all the boxes can be opened.
 
     Args:
-        boxes (list of list of int): A list of boxes, each containing a list of keys.
+        boxes (list of list of int):
+        A list of boxes, each containing a list of keys.
 
     Returns:
         bool: True if all boxes can be opened, else False.
     """
     n = len(boxes)
-    opened = [False] * n  # Initialize all boxes as unopened
-    opened[0] = True  # The first box is already open
-    keys = [0]  # Start with the key to the first box
+    opened = [False] * n
+    opened[0] = [True]
+    keys = [0]
 
     for key in keys:
         for box in boxes[key]:
@@ -24,4 +26,4 @@ def canUnlockAll(boxes):
                 opened[box] = True
                 keys.append(box)
 
-    return all(opened)  # Check if all boxes are opened
+    return all(opened)
