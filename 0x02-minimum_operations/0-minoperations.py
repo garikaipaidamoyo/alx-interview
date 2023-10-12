e6f57335ab9d6bf83e38d9a1dace2c16f21dfc0b
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-10-12 02:54:20 -0700

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""
+Module for minimum operations.
+"""
+
 
 def minOperations(n):
-    if n <= 1:
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The fewest number of operations to reach n H characters.
+
+    """
+    if not isinstance(n, int) or n <= 0:
         return 0
 
     operations = 0
@@ -9,14 +23,17 @@ def minOperations(n):
 
     while n > 1:
         while n % divisor == 0:
-            operations += divisor
             n //= divisor
+            operations += divisor
+
         divisor += 1
 
     return operations
 
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
