e96c90cb89a9e16d38216a0147f8e01c94979416
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-10-24 03:19:35 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,48 +1,32 @@
 #!/usr/bin/python3
-
+"""
+UTF-8 Validation
+"""
 
 def validUTF8(data):
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if byte & 0x80 == 0:
-            # 1-byte character, the leading bit is '0'
-            i += 1
-        elif byte & 0xE0 == 0xC0:
-            # 2-byte character, the leading bits are '110'
-            i += 1
-            if i >= len(data) or (data[i] & 0xC0 != 0x80):
+    # Create a mask for the most significant bit (MSB) of a byte
+    msb_mask = 1 << 7
+    # Create a mask to check if the byte is a multibyte character
+    multibyte_mask = 1 << 6
+    # Initialize the number of expected following bytes
+    num_following_bytes = 0
+
+    for byte in data:
+        # If we expect following bytes, check if the byte is a following byte
+        if num_following_bytes:
+            if byte & msb_mask and not byte & multibyte_mask:
+                num_following_bytes -= 1
+            else:
                 return False
-            i += 1
-        elif byte & 0xF0 == 0xE0:
-            # 3-byte character, the leading bits are '1110'
-            i += 1
-            for _ in range(2):
-                if i >= len(data) or (data[i] & 0xC0 != 0x80):
-                    return False
-                i += 1
-        elif byte & 0xF8 == 0xF0:
-            # 4-byte character, the leading bits are '11110'
-            i += 1
-            for _ in range(3):
-                if i >= len(data) or (data[i] & 0xC0 != 0x80):
-                    return False
-                i += 1
         else:
-            return False
+            # Determine the number of following bytes based on the MSB
+            while byte & msb_mask:
+                num_following_bytes += 1
+                byte <<= 1
 
-    return True
+            # For a single-byte character, num_following_bytes should be 0
+            if num_following_bytes == 1 or num_following_bytes > 4:
+                return False
 
-
-if __name__ == "__main__":
-    # You can add test cases here for testing
-    data1 = [65]
-    print(validUTF8(data1))  # Should print True
-
-    data2 = [80, 121, 116, 104, 111, 110, 32, 105,
-             115, 32, 99, 111, 111, 108, 33]
-
-    print(validUTF8(data2))  # Should print True
-
-    data3 = [229, 65, 127, 256]
-    print(validUTF8(data3))  # Should print False
+    # If all bytes were processed, and we expected 0 following bytes, it's valid
+    return num_following_bytes == 0
