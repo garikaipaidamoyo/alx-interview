dbd1c726db4b0888e26e43c39ea5c9d030e64165
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-10-24 03:53:35 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,30 +5,39 @@ UTF-8 Validation
 
 
 def validUTF8(data):
-    # Create a mask for the most significant bit (MSB) of a byte
-    msb_mask = 1 << 7
-    # Create a mask to check if the byte is a multibyte character
-    multibyte_mask = 1 << 6
     # Initialize the number of expected following bytes
     num_following_bytes = 0
 
     for byte in data:
-        # If we expect following bytes, check if the byte is a following byte
-        if num_following_bytes:
-            if byte & msb_mask and not byte & multibyte_mask:
+        # Check if the byte is a following byte
+        if 128 <= byte <= 191:
+            if num_following_bytes:
                 num_following_bytes -= 1
             else:
                 return False
         else:
             # Determine the number of following bytes based on the MSB
-            while byte & msb_mask:
-                num_following_bytes += 1
-                byte <<= 1
-
-            # For a single-byte character, num_following_bytes should be 0
-            if num_following_bytes == 1 or num_following_bytes > 4:
+            if 192 <= byte <= 223:
+                num_following_bytes = 1
+            elif 224 <= byte <= 239:
+                num_following_bytes = 2
+            elif 240 <= byte <= 247:
+                num_following_bytes = 3
+            elif byte > 247:
                 return False
 
     # If all bytes were processed, and we expected 0 following bytes,
     # it's valid
     return num_following_bytes == 0
+
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105,
+            115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
