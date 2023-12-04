5189887f50e8b24b965d1aa0e08b39d6bbf4777c
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-12-04 02:18:06 -0800

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,29 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    ben_wins = 0
+    maria_wins = 0
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if ben_wins > maria_wins:
+        return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
+    else:
+        return None
+
+
+print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))
